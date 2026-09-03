96a600cb04bbb17f1bd106ba0e5d09c1154d1277
viseriox-jpg <viseriox@gmail.com>
2026-09-03 15:31:10 -0300

    fix: use public background override

--- src/main/java/com/noveris/races/client/FakeNameScreen.java
+++ src/main/java/com/noveris/races/client/FakeNameScreen.java
@@ -16,7 +16,7 @@ public final class FakeNameScreen extends NoverisScreen {
 
     public FakeNameScreen() { super("APELIDO DE NOVERIS"); format=FakeNameClientState.format; color=FakeNameClientState.color; }
     /** A tela já desenha o painel opaco; o background padrão do Screen adicionaria blur por cima dele. */
-    @Override protected void renderBackground(GuiGraphics g, int mx, int my, float partial) { }
+    @Override public void renderBackground(GuiGraphics g, int mx, int my, float partial) { }
     @Override protected void init() {
         panelWidth = Math.min(width - 32, 1080); panelHeight = Math.min(height - 30, 650);
         left = (width - panelWidth) / 2; top = (height - panelHeight) / 2;

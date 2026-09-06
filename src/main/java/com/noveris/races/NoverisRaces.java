a3d00bf0d4d03109b48902393f1cd7c0a0c8521a
viseriox-jpg <viseriox@gmail.com>
2026-09-06 11:46:15 -0300

    Register server config through mod container

--- src/main/java/com/noveris/races/NoverisRaces.java
+++ src/main/java/com/noveris/races/NoverisRaces.java
@@ -3,14 +3,14 @@ package com.noveris.races;
 import com.noveris.races.network.RaceNetwork;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 
 @Mod(NoverisRaces.MOD_ID)
 public final class NoverisRaces {
     public static final String MOD_ID = "noveris_races";
-    public NoverisRaces(IEventBus modBus) {
+    public NoverisRaces(IEventBus modBus, ModContainer container) {
         modBus.addListener(RaceNetwork::register);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, RaceConfig.SPEC);
+        container.registerConfig(ModConfig.Type.SERVER, RaceConfig.SPEC);
     }
 }

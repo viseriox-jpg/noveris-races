623b464d66505ca6fafa932c55c8ea2ffc70fa05
viseriox-jpg <viseriox@gmail.com>
2026-09-06 11:41:00 -0300

    Register server balance configuration

--- src/main/java/com/noveris/races/NoverisRaces.java
+++ src/main/java/com/noveris/races/NoverisRaces.java
@@ -3,11 +3,14 @@ package com.noveris.races;
 import com.noveris.races.network.RaceNetwork;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.config.ModConfig;
 
 @Mod(NoverisRaces.MOD_ID)
 public final class NoverisRaces {
     public static final String MOD_ID = "noveris_races";
     public NoverisRaces(IEventBus modBus) {
         modBus.addListener(RaceNetwork::register);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, RaceConfig.SPEC);
     }
 }
